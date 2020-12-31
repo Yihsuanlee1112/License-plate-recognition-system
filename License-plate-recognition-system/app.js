cf2832e5f3b8e460f82b9b3dc6544e8a119102d1
Yihsuanlee1112 <yihsuan.rebecca.lee@gmail.com>
2020-12-31 14:30:20 +0800

    delete lines not needed

--- License-plate-recognition-system/app.js
+++ License-plate-recognition-system/app.js
@@ -37,15 +37,9 @@ app.use(express.static(path.join(__dirname,'../my-electron-app')));
 
 
 /*
-const express = require('express')
-const app = express()
-const port = 3000
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
 */
